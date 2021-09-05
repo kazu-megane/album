601dcf469bc97c43da4310aa955688c95434314b
kazu-megane <kazukazu.music1214@gmail.com>
2021-09-05 11:44:29 +0900

    [fix]script

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+const urlPrefix = process.env.URL_PREFIX ? "/" + process.env.URL_PREFIX : "";
+
 module.exports = {
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
   reactStrictMode: true,
   images: {
     domains: ["picsum.photos"],
