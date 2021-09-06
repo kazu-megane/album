f30ddbb67031d5357a0eaf26c449cb28e889d308
kazu-megane <kazukazu.music1214@gmail.com>
2021-09-06 19:16:22 +0900

    [fix]スライドイン検証

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import Head from "next/head";
+import { useState, useEffect } from "react";
 import style from "./style.module.scss";
 
+function createClassName(classNames: Array<string>) {
+  return classNames.join(" ");
+}
+
 const Home: NextPage = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 3000);
+  }, []);
+
   return (
     <div className={style.container}>
       <Head>
@@ -11,9 +24,17 @@ const Home: NextPage = () => {
         <meta name="description" content="Album" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-
       <main className={style.main}>
-        アルバムは<Link href="/album">こちらへ</Link>
+        <div
+          className={createClassName([
+            style.main__text,
+            isVisible ? style["main__text--visible"] : "",
+          ])}
+        >
+          <p className={style.main__textContent}>
+            アルバムは<Link href="/album">こちらへ</Link>
+          </p>
+        </div>
       </main>
     </div>
   );
