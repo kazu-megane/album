acdfb2f450a1342d8c93c77a0045d41abbc653e4
kazu-megane <kazukazu.music1214@gmail.com>
2021-09-21 21:55:51 +0900

    [fix]schedule

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,8 @@ const Schedule: FC = () => {
             ])}
           >
             鈴木愛香 生誕祭
+            <br />
+            presented by Kazuya.
           </p>
           <p className={style.Schedule__noteMessage}>
             開催日：9/23(木)〜9/24(金)
@@ -25,7 +27,7 @@ const Schedule: FC = () => {
               style["Schedule__noteMessage--new"],
             ])}
           >
-            持ち物：着替え、その他
+            持ち物：着替え、その他（わくわくした気持ちなど）
           </p>
           <p
             className={createClassName([
@@ -35,9 +37,19 @@ const Schedule: FC = () => {
           >
             備考：
             <br />
-            雨天結構ですので、準備はしっかりしておいてください。
+            雨天決行ですので、準備はしっかりとしておいてください。
             <br />
             夕食は17時30分からとなりますので、お腹は空かせといてください。
+            <br />
+            服装はカジュアルすぎない方が良いかと思います。
+          </p>
+          <p
+            className={createClassName([
+              style.Schedule__noteMessage,
+              style["Schedule__noteMessage--new"],
+            ])}
+          >
+            何か不明点がございましたら、お気軽にお聞きください。
           </p>
         </div>
       </div>
